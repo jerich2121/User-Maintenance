1051134fae8238ca51eade1754392f937da4b40f
Jerich <145184534+jerich2121@users.noreply.github.com>
2025-12-03 23:47:13 +0800

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Full Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(654, 26);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(47, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(654, 26);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Username:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(47, 134);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PasswordChar = '*';
+            this.textBox3.Size = new System.Drawing.Size(326, 26);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "New Password:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(379, 134);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.PasswordChar = '*';
+            this.textBox4.Size = new System.Drawing.Size(326, 26);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(376, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Confirm Password:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(47, 180);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Add";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(149, 180);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 34);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Update";
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(266, 180);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 34);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Delete";
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeight = 34;
+            this.dataGridView1.Location = new System.Drawing.Point(47, 220);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(654, 300);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(750, 550);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // Original connection string
+        private string connectionString = "Server=DT11GM0LS7E6;Database=UserDB;Trusted_Connection=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+            EnsureDatabaseAndTable(); // Create DB and table if they don't exist
+            LoadUsers();              // Load users into DataGridView
+        }
+
+        // Ensure Database and Users table exist
+        private void EnsureDatabaseAndTable()
+        {
+            // Step 1: Create Database if not exists
+            using (SqlConnection con = new SqlConnection("Server=DT11GM0LS7E6;Database=master;Trusted_Connection=True;"))
+            {
+                string createDB = @"
+                IF DB_ID('UserDB') IS NULL
+                CREATE DATABASE UserDB";
+                using (SqlCommand cmd = new SqlCommand(createDB, con))
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            // Step 2: Create Users table if not exists
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string createTable = @"
+                IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Users' AND xtype='U')
+                CREATE TABLE Users (
+                    Id INT PRIMARY KEY IDENTITY(1,1),
+                    FullName NVARCHAR(100),
+                    Username NVARCHAR(50),
+                    Password NVARCHAR(50)
+                );";
+                using (SqlCommand cmd = new SqlCommand(createTable, con))
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Load users into DataGridView
+        private void LoadUsers()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter("SELECT Id, FullName, Username FROM Users", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                dataGridView1.MultiSelect = false;
+            }
+        }
+
+        // Add User
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text != textBox4.Text)
+            {
+                MessageBox.Show("Passwords do not match!");
+                return;
+            }
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Users (FullName, Username, Password) VALUES (@FullName, @Username, @Password)";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@FullName", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@Username", textBox2.Text);
+                    cmd.Parameters.AddWithValue("@Password", textBox3.Text);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            MessageBox.Show("User added successfully!");
+            LoadUsers();
+            ClearFields();
+        }
+
+        // Update User
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Select a user to update.");
+                return;
+            }
+
+            if (textBox3.Text != textBox4.Text)
+            {
+                MessageBox.Show("Passwords do not match!");
+                return;
+            }
+
+            int userId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["Id"].Value);
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE Users SET FullName=@FullName, Username=@Username, Password=@Password WHERE Id=@Id";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@FullName", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@Username", textBox2.Text);
+                    cmd.Parameters.AddWithValue("@Password", textBox3.Text);
+                    cmd.Parameters.AddWithValue("@Id", userId);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            MessageBox.Show("User updated successfully!");
+            LoadUsers();
+            ClearFields();
+        }
+
+        // Delete User
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Select a user to delete.");
+                return;
+            }
+
+            int userId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["Id"].Value);
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Users WHERE Id=@Id";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@Id", userId);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            MessageBox.Show("User deleted successfully!");
+            LoadUsers();
+            ClearFields();
+        }
+
+        // Click row to fill textboxes
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                textBox1.Text = row.Cells["FullName"].Value.ToString();
+                textBox2.Text = row.Cells["Username"].Value.ToString();
+                textBox3.Text = "";
+                textBox4.Text = "";
+            }
+        }
+
+        // Clear textboxes
+        private void ClearFields()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void textBox2_TextChanged(object sender, EventArgs e) { }
+        private void textBox3_TextChanged(object sender, EventArgs e) { }
+        private void textBox4_TextChanged(object sender, EventArgs e) { }
+    }
+}
